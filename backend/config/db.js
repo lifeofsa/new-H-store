87788b75562f8dff1e1f93bebe2e2e4de83d6832
basiljamildev <thdev4.2@gmail.com>
2025-12-15 19:54:35 +0500

    NEW UPDATED

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,12 +3,16 @@ import colors from 'colors';
 import dotenv from 'dotenv';
 const DB = async () => {
   dotenv.config();
+  if (!process.env.MONGO_URI) {
+    console.error('MONGO_URI is undefined! Check your Vercel Environment Variables.');
+    return;
+  }
   try {
     const connect = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${connect.connection.host}`.cyan.underline);
   } catch (err) {
     console.error(`Error : ${err.message}`.red.underline.bold);
-    process.exit(1);
+    // process.exit(1); // Do not exit on Vercel
   }
 };
 export default DB;
