b76360ab9715b6845f40444606e157e73e1d39dd
basiljamildev <thdev4.2@gmail.com>
2025-12-15 18:27:23 +0500

    BUILD UPDATED IN .\package.json

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
 import axios from 'axios';
+import './bootstrap.min.css';
+import './index.css';
+import App from './App';
+import store from './store';
 
 axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:5000';
-
-import './bootstrap.min.css';
-import App from './App';
-
-import { Provider } from 'react-redux';
-import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
